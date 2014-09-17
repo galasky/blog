a055ca1bff202d2e41017c475e0d5cb0efa2ea54
lambert <ttrebmal@gmail.com>
2014-09-16 20:23:32 -0700

    function hello delete

--- blog.py
+++ blog.py
@@ -2,12 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/blog")
-def hello():
+def blog():
     return render_template("blog.html")
 
 @app.route("/")
-def hello():
-    return "Ya rien ici dégage ! Cordialement."
+def route():
+    return "Ya rien ici degage ! Cordialement."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
